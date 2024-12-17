bb03f562c08b61a4bf97ec2ed2ef3a0b0ba1ee28
DanielLopez <daniel.lopezing20@gmail.com>
2024-12-17 13:44:22 -0600

    Se agrega primer intento de solución del problema

--- ejercicios_iniciales/node_depths.py
+++ ejercicios_iniciales/node_depths.py
@@ -8,7 +8,21 @@ BinaryTree nodes themselves or None / null.
 
 def nodeDepths(root):
     # Write your code here.
+    arr = []
+    check_depth(root, 0, arr)
+    return sum(arr)
     pass
+
+
+def check_depth(node, current_depth_sum, depth_arr):
+    if node is None:
+        return
+    new_current_depth = current_depth_sum + 1
+    if node.left is None and node.right is None:
+        depth_arr.append(new_current_depth)
+        return
+    check_depth(node.left, new_current_depth, depth_arr)
+    check_depth(node.right, new_current_depth, depth_arr)
 
 
 # This is the class of the input binary tree.
