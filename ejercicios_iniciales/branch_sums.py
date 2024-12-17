62f67280d1b4f6c76403db3c951d8cae633f7fb5
DanielLopez <daniel.lopezing20@gmail.com>
2024-12-17 15:01:01 -0600

    Agrego soluciones de ejercicios de árboles nuevamente

--- ejercicios_iniciales/branch_sums.py
+++ ejercicios_iniciales/branch_sums.py
@@ -24,4 +24,18 @@ Result
 
 
 def branchSums(root):
-    pass
+    sums = []
+    calculate_branch_sums(root, 0, sums)
+    return sums
+
+
+def calculate_branch_sums(node, current_sum, sums_arr):
+    if node is None:
+        return
+    new_current_sum = current_sum + node.value
+
+    if node.left is None and node.right is None:
+        sums_arr.append(new_current_sum)
+        return
+    calculate_branch_sums(node.left, new_current_sum, sums_arr)
+    calculate_branch_sums(node.right, new_current_sum, sums_arr)
