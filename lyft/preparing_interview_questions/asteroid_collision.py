f990673f50df5fd4b6600706032e2cf2386fc431
DanielLopez <daniel.lopezing20@gmail.com>
2024-12-26 09:00:05 -0600

    agrego constraints de asteroides

--- lyft/preparing_interview_questions/asteroid_collision.py
+++ lyft/preparing_interview_questions/asteroid_collision.py
@@ -27,6 +27,13 @@ Input: asteroids = [10,2,-5]
 Output: [10]
 Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
 
+Constraints:
+
+    2 <= asteroids.length <= 104
+    -1000 <= asteroids[i] <= 1000
+    asteroids[i] != 0
+
+
 """
 
 
