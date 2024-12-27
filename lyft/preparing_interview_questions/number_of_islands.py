def9c919103b32c6b9ae2bbe7909b4c8c457870f
DanielLopez <daniel.lopezing20@gmail.com>
2024-12-26 18:43:29 -0600

    agrego solución de number of islands

--- lyft/preparing_interview_questions/number_of_islands.py
+++ lyft/preparing_interview_questions/number_of_islands.py
@@ -40,8 +40,26 @@ Constraints:
 
 
 def num_islands(grid):
-    """
-    :type grid: List[List[str]]
-    :rtype: int
-    """
-    pass
+    if not grid:
+        return 0
+
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '1':
+                dfs(grid, i, j)
+                count += 1
+    return count
+
+
+def dfs(grid, i, j):
+    if (i < 0 or j < 0
+            or i >= len(grid)
+            or j >= len(grid[0])
+            or grid[i][j] != '1'):
+        return
+    grid[i][j] = '#'
+    dfs(grid, i + 1, j)
+    dfs(grid, i - 1, j)
+    dfs(grid, i, j + 1)
+    dfs(grid, i, j - 1)
