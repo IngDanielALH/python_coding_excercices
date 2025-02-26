81cbe86d5c3625258452e21d061da0050a3f510a
Daniel López <daniel.lopezing20@gmail.com>
2025-02-26 17:16:03 -0600

    Agrego la información faltante del código del ejercicio de Lyft, también corrijo los datos para los tests

--- lyft/phone call/fetching.py
+++ lyft/phone call/fetching.py
@@ -11,7 +11,42 @@ will return a given number of results, abstracting away the need to do paginatio
 
 Your task will be to implement ResultFetcher.fetch()
 """
+from __future__ import annotations
+from typing import TypedDict
 
+
+# These numbers are for testing only and may be changed by the interviewer.
+# Do not use them in your solution
+
+MAX_RESULTS = 103
+PAGE_SIZE = 1
+
+class FetchPageResult(TypedDict):
+    next_page: int | None
+    results: list[int]
+
+# External API -- Should not be modified for solution
+def fetch_page(page: int) -> FetchPageResult:
+    """
+    Return the page of results and the next page. Pages are 0 indexed.
+
+    Returns:
+    {
+        "results": [...],
+        "next_page": 3
+    }
+    """
+    if page * PAGE_SIZE > MAX_RESULTS:
+        return {"next_page": None, "results": []}
+
+    return {
+        "next_page": page + 1,
+        "results": list(
+            range(page * PAGE_SIZE, min(MAX_RESULTS, (page + 1) * PAGE_SIZE))
+        ),
+    }
+
+################ Implement Solution here ################
 
 class ResultFetcher:
     def __init__(self) -> None:
