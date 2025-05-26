62200038a2a552ebe94bf7e121e761b1d8ed04db
DanielLopez <daniel.lopezing20@gmail.com>
2025-05-26 11:36:15 -0600

    agrego solución a social network

--- google_mock_interview/social_network.py
+++ google_mock_interview/social_network.py
@@ -1,2 +1,33 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+        self.size = [1] * (n + 1)
+
+    def find_parent(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find_parent(self.parent[x])  # Path compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        parentX = self.find_parent(x)
+        parentY = self.find_parent(y)
+
+        if parentX != parentY:
+            # Union by size
+            if self.size[parentX] < self.size[parentY]:
+                parentX, parentY = parentY, parentX
+            self.parent[parentY] = parentX
+            self.size[parentX] += self.size[parentY]
+
+    def get_size(self, x):
+        return self.size[self.find_parent(x)]
+
+
 def get_connections(u, v, nodes, queries):
+    uf = UnionFind(nodes)
+
+    for a, b in zip(u, v):
+        uf.union(a, b)
+
+    return [uf.get_size(user) for user in queries]
     pass
