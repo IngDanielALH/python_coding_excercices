3a507605b3a85ea419734b82b536e0ab413dc8bc
DanielLopez <daniel.lopezing20@gmail.com>
2025-05-26 11:41:36 -0600

    agrego ejercicio calculate password

---
+++ google_mock_interview/test_calculate_password.py
@@ -0,0 +1,22 @@
+import unittest
+from calculate_password import calculate_changes
+
+
+class testCalculatePassword(unittest.TestCase):
+    def test_1(self):
+        password = "abzzbz"
+        k = 3
+        expected = 1
+        self.assertEqual(expected, calculate_changes(password, k))
+
+    def test_2(self):
+        password = "cbpecbbc"
+        k = 4
+        expected = 2
+        self.assertEqual(expected, calculate_changes(password, k))
+
+    def test_3(self):
+        password = "vsvvsv"
+        k = 3
+        expected = 0
+        self.assertEqual(expected, calculate_changes(password, k))
