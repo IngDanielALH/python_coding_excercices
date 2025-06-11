e64a85833cad111ee2875e73121987e5c1440036
prinverdaniel <daniel.lopezing20@gmail.com>
2025-06-11 12:09:30 -0600

    Agrego ejercicio JuiceBottling.py

---
+++ ejercicios_nivel_dificil/test_juice_bottling.py
@@ -0,0 +1,10 @@
+import unittest
+from JuiceBottling import juiceBottling
+
+
+class testJuiceBottling(unittest.TestCase):
+    def test_case_1(self):
+        prices = [0, 2, 5, 6]
+        expected = [1, 2]
+        actual = juiceBottling(prices)
+        self.assertEqual(actual, expected)
