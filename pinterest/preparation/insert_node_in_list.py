79506612508f3ede96388324cb8bc283ff38e60b
prinverdaniel <daniel.lopezing20@gmail.com>
2025-08-13 20:50:32 -0600

    Agrego solución de insert_node_in_list

--- pinterest/preparation/insert_node_in_list.py
+++ pinterest/preparation/insert_node_in_list.py
@@ -44,12 +44,41 @@ class SinglyLinkedListNode:
         self.next = None
 
 
-def insertNodeAtPosition(llist, data, position):
+def insertNodeAtPosition(head, data, position):
+    p1 = None
+    p2 = head
+    current_position = 0
+    new_node = SinglyLinkedListNode(data)
+
+    while True:
+        if current_position == position:
+            if current_position == 0:
+                new_node.next = p2
+                break
+            else:
+                p1.next = new_node
+                new_node.next = p2
+                break
+        else:
+            p1 = p2
+            p2 = p1.next
+            current_position += 1
+    return head if current_position != 0 else new_node
     pass
 
 
-if __name__ == "__main__":
-    print("Inicio del programa")
+def print_linked_list(node):
+    """
+    Función auxiliar para imprimir la lista enlazada.
+    """
+    while node:
+        print(node.data, end=" -> ")
+        node = node.next
+    print("None")
+
+
+def test_1():
+    print("Inicio test_1")
     head = SinglyLinkedListNode(16)
     node_1 = SinglyLinkedListNode(13)
     node_2 = SinglyLinkedListNode(7)
@@ -57,4 +86,43 @@ if __name__ == "__main__":
     head.next = node_1
     node_1.next = node_2
 
-    insertNodeAtPosition(head, 1, 2)
+    print_linked_list(head)
+
+    new_list = insertNodeAtPosition(head, 1, 2)
+    print_linked_list(new_list)
+
+
+def test_2():
+    print("Inicio test_2")
+    head = SinglyLinkedListNode(16)
+    node_1 = SinglyLinkedListNode(13)
+    node_2 = SinglyLinkedListNode(7)
+
+    head.next = node_1
+    node_1.next = node_2
+
+    print_linked_list(head)
+
+    new_list = insertNodeAtPosition(head, 1, 0)
+    print_linked_list(new_list)
+
+
+def test_3():
+    print("Inicio test_3")
+    head = SinglyLinkedListNode(16)
+    node_1 = SinglyLinkedListNode(13)
+    node_2 = SinglyLinkedListNode(7)
+
+    head.next = node_1
+    node_1.next = node_2
+
+    print_linked_list(head)
+
+    new_list = insertNodeAtPosition(head, 1, 3)
+    print_linked_list(new_list)
+
+
+if __name__ == "__main__":
+    test_1()
+    test_2()
+    test_3()
