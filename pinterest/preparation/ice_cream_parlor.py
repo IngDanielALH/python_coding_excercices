9d588784a064a37cc422ebb665042aa41f329c7d
prinverdaniel <daniel.lopezing20@gmail.com>
2025-08-17 13:59:22 -0600

    Agrego ejercicio ice_cream_parlor

---
+++ pinterest/preparation/ice_cream_parlor.py
@@ -0,0 +1,14 @@
+"""
+Two friends like to pool their money and go to the ice cream parlor. They always choose two distinct flavors and
+they spend all of their money.
+
+Given a list of prices for the flavors of ice cream, select the two that will cost all of the money they have.
+
+Example. m = 6 cost = [1, 3, 4, 5, 6]
+
+The two flavors that cost 1 and 5 meet the criteria. Using 1-based indexing, they are at indices 1 and 4.
+"""
+
+
+def icecreamParlor(m, arr):
+    pass
