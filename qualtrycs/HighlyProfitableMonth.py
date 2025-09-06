53d9c77e4ae7c478a3f17dd6e860d1db479f91ff
prinverdaniel <daniel.lopezing20@gmail.com>
2025-09-06 13:39:08 -0600

    Agrego ejercicio 1 de la entrevista de qualtrycs con tests unitarios

---
+++ qualtrycs/HighlyProfitableMonth.py
@@ -0,0 +1,17 @@
+"""
+Se está examinando el rendimiento de las acciones de una empresa para evaluar su beneficio neto a lo largo del tiempo.
+
+Para un parámetro de análisis dado k, un intervalo de k meses consecutivos se considera altamente rentable si los
+precios de las acciones aumentan estrictamente a lo largo de esos meses. Dados los precios de las acciones para n meses
+y el parámetro de análisis k, determine el número de dichos intervalos altamente rentables.
+
+Ejemplo
+preciosAcciones = [5, 3, 5, 7, 8]
+k = 3
+
+Por lo tanto, la respuesta es 2.
+"""
+
+
+def analyzer(prices, k):
+    pass
