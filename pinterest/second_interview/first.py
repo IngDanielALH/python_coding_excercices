0f411ec76a0fb7bdadf30e44a10a932149b5eb0b
prinverdaniel <daniel.lopezing20@gmail.com>
2025-09-13 15:05:12 -0600

    Agrego primer ejercicio de la entrevista de pinterest del segundo round

---
+++ pinterest/second_interview/first.py
@@ -0,0 +1,9 @@
+"""
+Dada la entrada boards, generar una función que reciba pin_A y pin_B y que determine si hay alguna relación entre ellos
+"""
+
+BOARDS = {
+    "anime": {"isekai", "shonen", "horror"},
+    "movies": {"action", "horror"},
+    "animals": {"dog", "cat", "parrot"}
+}
